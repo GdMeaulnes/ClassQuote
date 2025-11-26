f2ee58ecbf3a5df8a4f1e48ec0804e569132ac1f
Richard DOUXAMI <richard.douxami@gmail.com>
2025-11-26 19:38:16 +0100

    V2 du 26 soir

--- ClassQuote/Model/Quote.swift
+++ ClassQuote/Model/Quote.swift
@@ -7,19 +7,28 @@
 
 import Foundation
 
-// Structure basée sur la doc de l'API 
+// Structure basée sur la doc de l'API Forismatic
 struct Quote: Codable {
-    let quoteText : String
+    let quoteText: String
     let quoteAuthor: String
-    let senderName : String
-    let senderLink : String
-    let quoteLink : String
+    let senderName: String
+    let senderLink: String
+    let quoteLink: String
 }
 
-// Sample quote
+// Structure basée sur la doc de UnSplash
+struct UnsplashPhoto: Codable {
+    let blur_hash: String
+    let urlsFull: String
+}
+
+// Samples quote&Photo
 let  sampleQuote: Quote = .init(quoteText: "We should all be thankful for those people who rekindle the inner spirit. ",
                                 quoteAuthor: "Albert Schweitzer ",
                                 senderName: "",
                                 senderLink: "",
                                 quoteLink: "http://forismatic.com/en/0555876a47/")
 
+let samplePhoto: UnsplashPhoto = .init(blur_hash: "LF8zMrE20f^kS$jYV@xtE2kC-oM{",
+                                       urlsFull: "https://images.unsplash.com/photo-1761429943764-94b69c3e9309?crop=entropy&cs=srgb&fm=jpg&ixid=M3w4MzUzMTR8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjQxODEyNzh8&ixlib=rb-4.1.0&q=85"
+                                       
--- ClassQuote/Model/UnSplashAPIDataSource.swift
+++ ClassQuote/Model/UnSplashAPIDataSource.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+// Erreurs courrantes à gérer
+enum UnSplashAPIDataSourceError: Error {
+    case invalidURL
+    case invalidResponse
+    case noData
+    case decodingError
+}
+
+// Interface avec l'API d'UnSplash
+class UnSplashAPIDataSource {
+
+    let session: URLSession
+    var baseURL: URL {
+        URL(string: "https://api.unsplash.com/photos/random/?client_id=YTnimpBsVVMoE8V3wMsSpz4a4TE68vbwCctRzm7UFCY")!
+    }
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    func getInfoPhoto() async throws -> UnsplashPhoto {
+        guard let (data, response) = try? await session.data(from: baseURL) else {
+            throw UnSplashAPIDataSourceError.noData
+        }
+
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw UnSplashAPIDataSourceError.invalidResponse
+        }
+
+        do {
+            return try JSONDecoder().decode(UnsplashPhoto.self, from: data)
+        } catch {
+            throw UnSplashAPIDataSourceError.decodingError
+        }
+    }
+}
